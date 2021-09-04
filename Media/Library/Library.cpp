0dd8de0beb6ba7e450d8e7d9b130417da1d47732
julien-guillan <guillan.julien@live.com>
2021-09-04 18:25:46 +0200

    Checked if file is a video file

--- Media/Library/Library.cpp
+++ Media/Library/Library.cpp
@@ -20,17 +20,19 @@ void Library::scanForFiles() {
     //TODO filesystem scan
     for(std::string& path : this->paths) {
         
-        for(auto& filePath : std::filesystem::directory_iterator(path)){
+        for(auto& filePath : std::filesystem::recursive_directory_iterator(path)){
             //TODO check if file is video 
-            magic_t myt = magic_open(MAGIC_CONTINUE | MAGIC_ERROR | MAGIC_DEBUG | MAGIC_MIME);
+            magic_t myt = magic_open(MAGIC_CONTINUE | MAGIC_ERROR | MAGIC_MIME);
             magic_load(myt, NULL);
-            auto file = magic_file(myt, NULL);
+            const char* file = magic_file(myt, filePath.path().c_str());
             if (file == nullptr) {
                 std::cerr << magic_error(myt) << std::endl;
             } else {
-                std::cout << file << ' ' << file << std::endl;
+                std::string fileStr = std::string(file); 
+                if(fileStr.find("video") != std::string::npos) {
+                    std::cout << fileStr << ' ' << filePath << std::endl;
+                }
             }
-            std::cout << filePath << ": " << file << std::endl;
             magic_close(myt);
         }
     }
